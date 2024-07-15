1ff60345ce8973402959c4f81d6b7a2041d2120f
Luis Ángel Díaz <luisangel.ds@gmail.com>
2024-07-15 09:07:17 -0600

    Palindrome Project

---
+++ javascript /Palindrome/script.js
@@ -0,0 +1,9 @@
+const checkBtn = document.getElementById('check-btn');
+const inputWord = document.getElementById('text-input');
+
+checkBtn.addEventListener('click', () => {
+  
+  alert("Click me")
+  console.log(inputWord);
+  
+})
